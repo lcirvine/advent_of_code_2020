c19ac7faa691731e3cfb0d59f124209fd0c8a31c
lcirvine <55790560+lcirvine@users.noreply.github.com>
2020-12-08 16:35:12 +0000

    Adding day 5

--- day_5.py
+++ day_5.py
@@ -5,13 +5,12 @@ with open(os.path.join('Inputs', 'day_5.txt')) as f_input:
     data = f_input.read().split('\n')
 
 
-def halver(num, letter, divisions: int = 2):
-    half_1 = num // divisions
-    half_2 = num - half_1
-    if letter in ['F', 'L']:
-        return half_1
-    elif letter in ['B', 'R']:
-        return half_2
+def return_range(char: str, num_range: range, divisions: int = 2):
+    half_range = len(num_range) // divisions
+    if char in ['F', 'L']:
+        return num_range[:half_range]
+    elif char in ['B', 'R']:
+        return num_range[half_range:]
 
 
 def part_1():
@@ -24,14 +23,29 @@ def part_1():
         R means to take the upper half (i.e. higher numbers)
     :return:
     """
-    def row():
-        pass
+    seats = []
+    for seat in data:
+        row_str = seat[:7]
+        row_range = range(0, 128)
+        for char in row_str:
+            row_range = return_range(char, row_range)
+        col_str = seat[7:]
+        col_range = range(0, 8)
+        for char in col_str:
+            col_range = return_range(char, col_range)
+        seat_id = (row_range[0] * 8) + col_range[0]
+        seats.append(seat_id)
+    return seats
 
-    def col():
-        pass
 
-    seat
+def part_2():
+    seats = part_1()
+    max_seat_id = (128 * 8) + 7
+    seat_id_range = range(0, max_seat_id)
+    missing_seats = [s for s in seat_id_range if s not in seats]
+    return [x for x in missing_seats if x + 1 not in missing_seats and x - 1 not in missing_seats]
 
 
 if __name__ == '__main__':
-    part_1()
+    print(max(part_1()))
+    print(part_2())
