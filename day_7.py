1d5ef9e03e10ed544ab61feaf15f38d7bf213c13
lcirvine <lance.c.irvine@gmail.com>
2020-12-24 13:11:33 +0000

    Finished day 7 with significant help

--- day_7.py
+++ day_7.py
@@ -1,64 +1,51 @@
 import os
 import re
+import collections
 
 with open(os.path.join('Inputs', 'day_7.txt')) as f:
     rules = f.read().split('\n')
 
-bag_dict = {}
+contained_in = collections.defaultdict(set)
+contains = collections.defaultdict(list)
 bag_pat = re.compile(r'^(\d)*\s([a-z\s]*)\sbag')
 for rule in rules:
     bag, content_str = rule.split(' bags contain ')
     content_str = [c.replace('.', '').strip() for c in content_str.split(',')]
-    contents = {}
     for c in content_str:
         if c == 'no other bags':
-            contents = None
+            pass
         else:
             num, bag_inside = re.search(bag_pat, c).groups()
-            contents[bag_inside] = int(num)
-    bag_dict[bag] = contents
+            contains[bag].append((int(num), bag_inside))
+            contained_in[bag_inside].add(bag)
 
 
-def return_contents(item, sg_count):
-    sg_count += len([x for x in bag_dict[item] if x == 'shiny gold'])
-    for i in bag_dict[item]:
-        if bag_dict[i] is not None:
-            return_contents(i, sg_count)
-    return sg_count
+holdsgold = set()
 
 
-def return_parents(item, sg_count, return_list):
-    for k, v in bag_dict.items():
-        if v is not None and item in v:
-            return_list.append(k)
-            return_parents(k, sg_count, return_list)
-            print(k)
-    return return_list
+def check(color):
+    for c in contained_in[color]:
+        holdsgold.add(c)
+        check(c)
 
 
 def part_1():
-    print(len(return_parents('shiny gold', 0, ['shiny gold'])))
+    check('shiny gold')
+    print(len(holdsgold))
 
 
-def part_1_test():
-    bag_dict = {}
-    bag_pat = re.compile(r'^(\d)*\s([a-z\s]*)\sbag')
-    for rule in rules:
-        bag, content_str = rule.split(' bags contain ')
-        content_str = [c.replace('.', '').strip() for c in content_str.split(',')]
-        contents = {}
-        for c in content_str:
-            if c == 'no other bags':
-                contents = None
-            else:
-                num, bag_inside = re.search(bag_pat, c).groups()
-                contents[bag_inside] = int(num)
-        bag_dict[bag] = contents
+def cost(color):
+    total = 0
+    for ct, inner in contains[color]:
+        total += ct
+        total += ct * cost(inner)
+    return total
+
 
 def part_2():
-    pass
+    print(cost('shiny gold'))
 
 
 if __name__ == '__main__':
     part_1()
-    # print(part_2())
+    part_2()
